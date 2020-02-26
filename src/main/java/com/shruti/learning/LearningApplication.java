283c2267f086cac6fe7108713c978a6815070dd1
shrutivilankar <shruti.vilankar@gmail.com>
2020-02-26 15:47:48 +0000

    create simple project

---
+++ src/main/java/com/shruti/learning/LearningApplication.java
@@ -0,0 +1,14 @@
+package com.shruti.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication{
+
+    public  static void main(String[] args){
+        SpringApplication.run(LearningApplication.class, args);
+        System.out.println("Create app from scratch...Hurrayyyy");
+    }
+
+}
